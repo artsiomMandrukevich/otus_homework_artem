9f41193b2b1d590cdb62034c3a7e46f5dfc53f3f
Artem Mandrukevich <artemmandrukevich@coherentsolutions.com>
2021-11-03 21:32:10 +0300

    First commit

--- 2_home_work/tests/test_6_set.py
+++ 2_home_work/tests/test_6_set.py
@@ -7,3 +7,5 @@ s2 = {1, 2, 3, 4}
 def test_6_set(fixture_greeting_function, fixture_greeting_module, fixture_greeting_session):
     """"Тест проверяет, что два множества равны"""
     assert (s1 == s2)
+
+# test sommit
